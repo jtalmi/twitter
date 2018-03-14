56754538e0487ce89b83149c8075dcc9c8d15833
Jonathan Talmi <jtalmi@gmail.com>
2018-03-14 02:28:39 +0000

    removed credentials

--- credentials.py
+++
@@ -1,8 +0,0 @@
-credentials = {
-    'consumer_key': "YAYQVfgLhNKhfCBQgOUmWXHMI",
-    'consumer_secret': "xR0VcKGaKSXlaA2EgAiItA6tY2XrxDJ503ucPXhNnRwfLa7bNW",
-    'access_token': "969664006247124992-rx6K00UGpg29O253ExsmftwJpys57yC",
-    'access_token_secret': "jbZx5lrZSF701L7lwPNqTw3OM5ggFsxDtOWq99k8GCdCI",
-    'username': 'elastic'
-    'password': 'bankoftwitteradmin'
-}
