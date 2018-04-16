d2aab22d9cd5ff4ab34d0824ab2a369cbeb4d70e
Jonathan <jtalmi@gmail.com>
2018-04-15 20:22:21 -0400

    improved the categorize tweets function and removed some extra logic

--- twitter_streamer_utils.py
+++ twitter_streamer_utils.py
@@ -3,19 +3,29 @@ from elasticsearch.exceptions import TransportError
 import re
 
 terms = ['reserve bank', 'glenn stevens', 'graeme wheeler', 'philip lowe', 'phillip lowe', 'adrian orr',
-            'bank of canada', 'poloz', 'boc rate', 'boc inflation', 'boc monetary', 'boc financial',
-            'ecb', 'draghi', 'european central bank',
-            'bank of england', 'mark carney', 'boe rate', 'boe inflation', 'boe monetary', 'boe financial',
-            'fed', 'federal reserve', 'FOMC', 'yellen', 'powell',
-            'bank of japan', 'kuroda', 'boj rate', 'boj inflation', 'boj monetary', 'boj financial']
+        'bank of canada', 'poloz', 'boc rate', 'boc inflation', 'boc monetary', 'boc financial',
+        'ecb', 'draghi', 'european central bank',
+        'bank of england', 'mark carney', 'boe rate', 'boe inflation', 'boe monetary', 'boe financial',
+        'fed', 'federal reserve', 'FOMC', 'yellen', 'powell',
+        'bank of japan', 'kuroda', 'boj rate', 'boj inflation', 'boj monetary', 'boj financial']
+
+groups = {'aus', 'boc', 'ecb', 'boe', 'fed', 'boj'}
+groups['aus'] = ['reserve bank', 'glenn stevens', 'graeme wheeler', 'philip lowe', 'phillip lowe', 'adrian orr']
+groups['boc'] = ['bank of canada', 'poloz', 'boc rate', 'boc inflation', 'boc monetary', 'boc financial']
+groups['ecb'] = ['ecb', 'draghi', 'european central bank']
+groups['boe'] = ['bank of england', 'mark carney', 'boe rate', 'boe inflation', 'boe monetary', 'boe financial']
+groups['fed'] = ['fed', 'FOMC', 'yellen', 'powell']
+groups['boj'] = ['bank of japan', 'kuroda', 'boj rate', 'boj inflation', 'boj monetary', 'boj financial']
 
 banks = {}
-banks['aus'] = [re.compile('^(?=.*reserve)(?=.*bank).*$'), re.compile('^(?=.*glenn)(?=.*stevens).*$'), re.compile('^(?=.*graeme)(?=.*wheeler).*$'), re.compile('^(?=.*philip)(?=.*lowe).*$'), re.compile('^(?=.*phillip)(?=.*lowe).*$')]
+banks['aus'] = [re.compile('^(?=.*reserve)(?=.*bank).*$'), re.compile('^(?=.*glenn)(?=.*stevens).*$'), re.compile('^(?=.*graeme)(?=.*wheeler).*$'), re.compile('^(?=.*phill?ip)(?=.*lowe).*$')]
 banks['boc'] = [re.compile('^(?=.*bank)(?=.*of)(?=.*canada).*$'), 'poloz', re.compile('^(?=.*boc)(?=.*(inflation|rate|monetary|financial)).*$')]
 banks['ecb'] = ['ecb', 'draghi', re.compile('^(?=.*european)(?=.*central)(?=.*bank).*$')]
 banks['boe'] = [re.compile('^(?=.*bank)(?=.*of)(?=.*england).*$'), re.compile('^(?=.*mark)(?=.*carney).*$'), re.compile('^(?=.*boe)(?=.*(inflation|rate|monetary|financial)).*$')]
-banks['fed'] = ['fed', re.compile('^(?=.*federal)(?=.*reserve).*$'), 'fomc', 'yellen', 'powell']
+banks['fed'] = ['fed', 'fomc', 'yellen', 'powell']
 banks['boj'] = [re.compile('^(?=.*bank)(?=.*of)(?=.*japan).*$'), 'kuroda', re.compile('^(?=.*boj)(?=.*(inflation|rate|monetary|financial)).*$')]
+
+
 
 def create_twitter_index(client, index='twitter'):
     create_index_body = {
@@ -61,24 +71,25 @@ def categorize_tweet(tweet, banks):
     """
     categories = []
     text = ""
-
-    if 'retweeted_status' in tweet.keys():
-        try:
-            text += tweet['retweeted_status']['extended_tweet']['full_text'].lower()
-        except:
-            text += tweet['retweeted_status']['text'].lower()
-    if 'quoted_status' in tweet.keys():
-        try:
-            text += tweet['quoted_status']['extended_tweet']['full_text'].lower()
-        except:
-            text += tweet['quoted_status']['text'].lower()
-    try:
-        text += tweet['extended_tweet']['full_text'].lower()
-    except:
-        text += tweet['text_lower']
+    if tweet['retweeted_status']:
+        if tweet['retweeted_status']['extended_tweet']:
+            text += tweet['retweeted_status']['extended_tweet']['full_text'] + '\s'
+        else:
+            text += tweet['retweeted_status']['text'] + '\s'
+    if tweet['is_quote_status']:
+        if tweet['quoted_status']['extended_tweet']['full_text']:
+            text += tweet['quoted_status']['extended_tweet']['full_text'] + '\s'
+        else:
+            text += tweet['quoted_status']['text'] + '\s'
+    if tweet['extended_tweet']:
+        text += tweet['extended_tweet'] + '\s'
+    else:
+        text += tweet['text'] + '\s'
 
     for bank, keywords in banks.iteritems():
         for pattern in keywords:
-            if bool(re.search(pattern, text)):
+            if bool(re.search(pattern, text, re.IGNORECASE)):
                 categories.append(bank)
+                continue
+
     return categories
